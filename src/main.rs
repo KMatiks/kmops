b3c92bc7ab9f291e891a64f937de9ad3c423af6e
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-12-03 19:25:34 -0500

    Remove unneeded code. Fix double declaration of println macro

--- src/main.rs
+++ src/main.rs
@@ -3,61 +3,10 @@
 
 #![feature(custom_test_frameworks)]
 #![test_runner(kmops::test_runner)]
-
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use kmops::println;
-
-mod vga_buffer;
-mod serial;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T where T: Fn(), {
-    fn run(&self) -> () {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    kmops::test_panic_handler(info);
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -72,11 +21,20 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
 
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    kmops::test_panic_handler(info);
 }
