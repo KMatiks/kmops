874b4337704ff73097570721f815d5d5e2075329
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-12-03 19:25:03 -0500

    Reshuffle prelude location

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 #![no_std]
-
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use core::panic::PanicInfo;
+
 pub mod serial;
 pub mod vga_buffer;
-
-use core::panic::PanicInfo;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
