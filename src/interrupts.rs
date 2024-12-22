a0ef918fed7d3bb323dc0df6494a9bbe3258e5c5
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-12-21 22:09:38 -0500

    Utilize library for parsing PS/2 scancodes

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -2,11 +2,19 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::{println, print, gdt};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
-use spin;
+use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+use spin::Mutex;
 
 // Primary PIC offsets
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+lazy_static! {
+    static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+        Mutex::new(Keyboard::new(ScancodeSet1::new(),
+            layouts::Us104Key, HandleControl::Ignore)
+        );
+}
 
 // Initialize the PICs in a thread-safe manner using a Mutex spinlock
 // Tell the PICs which offsets to start from for their interrupt vectors
@@ -69,25 +77,17 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
     use x86_64::instructions::port::Port;
 
+    let mut keyboard = KEYBOARD.lock();
     let mut port = Port::new(0x60);
     let scancode: u8 = unsafe { port.read() };
 
-    let key = match scancode {
-        0x02 => Some('1'),
-        0x03 => Some('2'),
-        0x04 => Some('3'),
-        0x05 => Some('4'),
-        0x06 => Some('5'),
-        0x07 => Some('6'),
-        0x08 => Some('7'),
-        0x09 => Some('8'),
-        0x0a => Some('9'),
-        0x0b => Some('0'),
-        _ => None,
-    };
-
-    if let Some(key) = key {
-        print!("{}", key);
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
     }
 
     unsafe {
