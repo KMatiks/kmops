5b45c91c45b1600dcb217d00f0a87149e4eccbe7
asdf57 <53836417+asdf57@users.noreply.github.com>
2025-01-04 10:42:08 -0500

    Add page usage query function

--- src/memory.rs
+++ src/memory.rs
@@ -2,7 +2,7 @@ use x86_64::{
     structures::paging::PageTable,
     VirtAddr,
 };
-
+use x86_64::structures::paging::{Page, PageTableFlags};
 use crate::{println};
 
 pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
@@ -19,46 +19,69 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
 
 pub unsafe fn calculate_memory_usage(physical_memory_offset: VirtAddr) -> () {
     let pml4_table = active_level_4_table(physical_memory_offset);
-    
-    const MAX_ENTRIES: u16 = 512;
-    const PAGES_PER_L4_ENTRY: u32 = 1<<27;
-    const TOTAL_PAGES: u64 = 1<<36;
 
-    let mut num_l4_unused = 0;
-    let mut num_l3_unused = 0;
-    let mut num_l2_unused = 0;
+    const MAX_ENTRIES: u16 = 1 << 9;
+    const MAX_L3_ENTRIES: u64 = 1 << 18;
+    const MAX_L2_ENTRIES: u64 = 1 << 27;
+    const MAX_PAGE_COUNT: u64 = 1 << 36;
+
+    let mut num_l4_unused: u16 = 0;
+    let mut num_l3_unused: u64 = 0;
+    let mut num_l2_unused: u64 = 0;
+    let mut num_l1_unused: u64 = 0;
 
     for (i, entry) in pml4_table.iter().enumerate() {
         if entry.is_unused() {
             num_l4_unused += 1;
+            continue;
         }
 
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
+        let phys = entry.frame().unwrap().start_address();
+        let va = physical_memory_offset + phys.as_u64();
+        let ptr = va.as_mut_ptr();
+        let l3_table: &PageTable = unsafe { &*ptr };
 
-            let phys = entry.frame().unwrap().start_address();
-            let ptr = physical_memory_offset + phys.as_u64();
-            let l3_table: &PageTable = unsafe { &*ptr };
+        for (i, entry) in l3_table.iter().enumerate() {
+            if entry.is_unused() {
+                num_l3_unused += 1;
+                continue;
+            }
 
-            for (i, entry) in l3_table.iter().enumerate() {
-                println!("L3 Entry {}: {:?}", i, entry);
+            if !entry.is_unused() {
+                let pa = entry.frame().unwrap().start_address();
+                let va = physical_memory_offset + pa.as_u64();
+                let ptr = va.as_mut_ptr();
+                let l2_table: &PageTable = unsafe { &*ptr };
 
-                if !entry.is_unused() {
+                for (i, entry) in l2_table.iter().enumerate() {
+                    if entry.is_unused() {
+                        num_l2_unused += 1;
+                        continue;
+                    }
+
+                    if entry.flags().contains(PageTableFlags::HUGE_PAGE) {
+                        continue;
+                    }
+
                     let pa = entry.frame().unwrap().start_address();
                     let va = physical_memory_offset + pa.as_u64();
-                    let ptr = VirtAddr::new(va).as_mut_ptr();
-                    let l4_table: &PageTable = unsafe { &*ptr };
+                    let ptr = va.as_mut_ptr();
+                    let l1_table: &PageTable = unsafe { &*ptr };
 
-                    for (i, entry) in l4_table.iter().enumerate() {
-                        println!("L4 Entry {}: {:?}", i, entry);
+                    for (i, entry) in l1_table.iter().enumerate() {
+                        if entry.is_unused() {
+                            num_l1_unused += 1;
+                            continue;
+                        }
                     }
                 }
             }
         }
-
-
     }
 
-    println!("{}/512 L4 entries unused", num_l4_unused);
     println!("{}/512 L4 entries used", MAX_ENTRIES - num_l4_unused);
+    println!("{}/{} L3 entries used", MAX_L3_ENTRIES - num_l3_unused, MAX_L3_ENTRIES);
+    println!("{}/{} L2 entries used", MAX_L2_ENTRIES - num_l2_unused, MAX_L2_ENTRIES);
+    println!("{}/{} L1 entries used", MAX_PAGE_COUNT - num_l1_unused, MAX_PAGE_COUNT);
+
 }
