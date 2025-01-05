01b78ef3c66dcb030617fac9c12eeacc28c578b2
asdf57 <53836417+asdf57@users.noreply.github.com>
2025-01-04 20:17:18 -0500

    Remove test page to frame mapping logic

--- src/main.rs
+++ src/main.rs
@@ -23,35 +23,6 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         println!("Memory region: {:?}", entry);
     }
 
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe {
-        memory::BootInfoFrameAllocator::init(&boot_info.memory_map)
-    };
-
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
-
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
-
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = unsafe { mapper.translate_addr(virt) };
-        println!("{:?} -> {:?}", virt, phys);
-    }
-
     #[cfg(test)]
     test_main();
 
